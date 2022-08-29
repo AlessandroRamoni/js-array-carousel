179364d21ff67bfcaa0beb30b23b4d3b535c429c
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-29 16:48:38 +0200

    create function "click"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("Lunedì");
+
+const next = document.getElementById("next");
+const back = document.getElementById("back");
+
+next.addEventListener("click", function () {
+  console.log("mi hai cliccato");
+  const activeItem = document.querySelector(".immagini.active");
+  const nextElemet = activeItem.nextElementSibling;
+  if (nextElemet) {
+    activeItem.classList.remove("active");
+    nextElemet.classList.add("active");
+  }
+});
